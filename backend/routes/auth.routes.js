5392338fe0509140cdadade2be1cb508ad702bf8
ssr0016 <samsonrecluta0016@gmail.com>
2025-02-21 14:54:58 +0800

    1 - auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,15 +1,14 @@
  import express from 'express'
- import { login, logout, signup } from '../controllers/auth.controller.js'
+ import { getMe, login, logout, signup } from '../controllers/auth.controller.js'
+import { protectRoute } from '../middleware/protectRoute.js';
 
  const router = express.Router()
 
+
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login)
-
 router.post("/logout", logout)
 
  export default router;
 
- // mongo db password
-// PEgiNAXewTtAbPrL

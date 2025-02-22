dbd77d4034bfbc87008a321c9ad12438874d0922
ssr0016 <samsonrecluta0016@gmail.com>
2025-02-22 09:32:35 +0800

    1 - user routes and controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -4,7 +4,6 @@ import { protectRoute } from '../middleware/protectRoute.js';
 
  const router = express.Router()
 
-
 router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login)
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
-import express from 'express'
-import authRoutes from './routes/auth.routes.js'
-import dotenv from 'dotenv'
-import connectMongoDB from './db/connectMongoDB.js'
+import express from 'express';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from 'cloudinary';
+
+import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+import connectMongoDB from './db/connectMongoDB.js'
 
 dotenv.config();
+
+// for img upload
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +26,7 @@ app.use(express.urlencoded({ extended: true })); // to parse form data(urlencode
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
